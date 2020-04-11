ec1709441a159197b54eaff8f41c0faff855ab18
Rodrigo Juarez <rodrigomjuarez@gmail.com>
2020-04-11 12:02:48 -0300

    Compact overview

---
+++ CodingCoach/CodingCoach/CustomControls/MentorDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CodingCoach.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MentorDetail : ContentView
+    {
+        public MentorDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CodingCoach/CodingCoach/CustomControls/MentorOverview.xaml.cs
@@ -0,0 +1,25 @@
+﻿using CodingCoach.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CodingCoach.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MentorOverview
+    {
+        public MentorOverview()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty MentorProperty = BindableProperty.Create(
+            "Mentor",
+            typeof(MentorDto),
+            typeof(MentorOverview));
+
+        public MentorDto Mentor { 
+            get => (MentorDto)GetValue(MentorProperty);
+            set => SetValue(MentorProperty, value);
+        }
+    }
+}
