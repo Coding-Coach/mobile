3e53246ee900b45c7bdbdce1417c0399a3b593b1
Rodrigo Juarez <rodrigomjuarez@gmail.com>
2020-04-10 23:40:34 -0300

    Fix avatar url

--- CodingCoach/CodingCoach/Models/Mentor.cs
+++ CodingCoach/CodingCoach/Models/Mentor.cs
@@ -15,6 +15,7 @@ namespace CodingCoach.Models
       public ObservableCollection<Channel> Channels           { get; set; }
       public string                        CountryImageSource => $"https://www.countryflags.io/{Country}/flat/32.png";
       public bool                          IsFavorite         { get; set; }
+      public bool                          IsAvailable         { get; set; }
       public string FavoriteColor => IsFavorite
                                         ? "Red"
                                         : "Gray";
@@ -30,11 +31,29 @@ namespace CodingCoach.Models
    {
        public static MentorDto ToMentorDto(this Mentor mentor)
        {
+           // TODO: languages
            var mentorDto = new MentorDto();
 
+           mentorDto.Id = mentor.Id;
+           mentorDto.IsAvailable = mentor.Available;
+           mentorDto.Tags = new ObservableCollection<string>(mentor.Tags);
            mentorDto.Name = mentor.Name;
+           mentorDto.Avatar = mentor.Avatar.ToAvatarUrl();
+           mentorDto.Title = mentor.Title;
+           mentorDto.Description = mentor.Description;
+           mentorDto.Country = mentor.Country;
 
            return mentorDto;
+       }
+
+       public static string ToAvatarUrl(this string url)
+       {
+           if (url.StartsWith("/avatars/"))
+           {
+               return $"{new DotNetClient.Configuration().Url}{url}";
+           }
+
+           return url;
        }
    }
 }
